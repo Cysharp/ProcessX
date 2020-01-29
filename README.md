fc09b73b7467af25539320c1121b7bfacc0343e6
Yoshifumi Kawai <ils@neue.cc>
2020-01-29 23:11:36 +0900

    got it

--- README.md
+++ README.md
@@ -2,4 +2,11 @@ ProcessX
 ===
 [![CircleCI](https://circleci.com/gh/Cysharp/ProcessX.svg?style=svg)](https://circleci.com/gh/Cysharp/ProcessX)
 
-WIP
+```csharp
+using Cysharp.Diagnostics; // using namespace
+
+await foreach (var item in ProcessX.StartAsync("dotnet build --help"))
+{
+    Console.WriteLine(item);
+}
+```
--- sandbox/ConsoleApp/Program.cs
+++ sandbox/ConsoleApp/Program.cs
@@ -1,18 +1,14 @@
-﻿using Cysharp.Diagnostics;
+﻿using Cysharp.Diagnostics; // using namespace
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+class Program
 {
-    class Program
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        await foreach (var item in ProcessX.StartAsync("dotnet build --help"))
         {
-            await foreach (var item in ProcessX.StartAsync("dotnet build --help"))
-            {
-                Console.WriteLine(item);
-            }
+            Console.WriteLine(item);
         }
     }
 }
